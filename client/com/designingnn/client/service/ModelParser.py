e1e24c8ad0f3007f7d91df41991c2820a4e5f8a1
Sai Krishna Kishore <beathanabhotla.kishore@gmail.com>
2018-10-27 07:32:51 +0530

    client side model parsing code

---
+++ client/com/designingnn/client/service/ModelParser.py
@@ -0,0 +1,6 @@
+class ModelParser:
+    def __init__(self):
+        pass
+
+    def parse_model(self, model_def):
+        pass
--- client/com/designingnn/client/service/ModelTrainService.py
+++ client/com/designingnn/client/service/ModelTrainService.py
@@ -1,8 +1,8 @@
-import asyncio
 
 class ModelTrainService:
     def __init__(self):
         pass
 
     def train_model(self, model_options):
+        model_options['model_def']
         pass
